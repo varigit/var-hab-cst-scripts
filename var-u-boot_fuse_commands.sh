aae5f8466344577147758327a73a4c6d6cb37834
Eran Matityahu <eran.m@variscite.com>
2017-12-20 13:32:55 +0200

    Adjust directory structure to CST 2.3.3
    In NXP Code Signing Tool (CST) 2.3.3 the binaries
    have moved from linux64/ to linux64/bin/.
    This script repository should be added and pulled
    to the binaries directory, and this commit adjusts
    the scripts for the new directory structure.
    
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- var-u-boot_fuse_commands.sh
+++ var-u-boot_fuse_commands.sh
@@ -23,6 +23,6 @@ do
 		word=0
 	fi
 	offset=$((i * 4))
-	printf "fuse prog -y $bank $word %s\n" `hexdump -s $offset -n 4  -e '/4 "0x"' -e '/4 "%X""\n"' ../crts/SRK_1_2_3_4_fuse.bin`
+	printf "fuse prog -y $bank $word %s\n" `hexdump -s $offset -n 4  -e '/4 "0x"' -e '/4 "%X""\n"' ../../crts/SRK_1_2_3_4_fuse.bin`
 	((word++))
 done
